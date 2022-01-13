7c6a8ce0c3954e3730e11aec807a451179dbbc4e
nathan2626 <nathanjourno@yahoo.fr>
2022-01-13 17:28:57 +0100

    Add migrations and seeders

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $token = md5(uniqid(true));
+
+            DB::table('posts')->insert([
+                'body' => $faker->paragraph,
+                'image' => '1.png',
+                'user_id' => 1,
+                'likes' => 43
+            ]);
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+
+            $token = md5(uniqid(true));
+
+            DB::table('users')->insert([
+                'email' => $faker->email,
+                'token' => $token,
+                'name' => $faker->username,
+                'image' => '1.png',
+                'password' => 'Test1'
+            ]);
+        }
+    }
+}

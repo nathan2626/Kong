7d6940c95c708578ef0194358bcf7ec46b028596
nathan2626 <nathanjourno@yahoo.fr>
2022-01-13 16:55:56 +0100

    Creation of database tables

--- database/migrations/2022_01_13_155204_create_comments_table.php
+++ database/migrations/2022_01_13_155204_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('image');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('body');
+            $table->foreignId('user_id');
+            $table->foreignId('post_id');
+            $table->bigInteger('likes')->default('0');;
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }

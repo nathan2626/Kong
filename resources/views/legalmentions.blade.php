50a5cdda253549e65cae2897919da09854f20964
Andrea Musso <andreamusso@MacBook-Pro-de-Andrea.local>
2022-01-14 13:12:28 +0100

    erreur git nettoyage

--- resources/views/legalmentions.blade.php
+++ resources/views/legalmentions.blade.php
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-@extends('layouts.navigation')
-
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Mentions Légales') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
-=======
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -32,4 +11,4 @@
 @extends('layouts.navigation')
 </body>
 </html>
->>>>>>> 4c497c323467b16c72ed2620ba1c4040626358c1
+
--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
-@extends('layouts.navigation')
-
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Timeline') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-=======
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">

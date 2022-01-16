33bd572735e4b1bb5c2d65320060a838f60760fa
AdamAlloucheDev <adam.allouchepro@gmail.com>
2022-01-17 00:32:14 +0100

    correction bug d'affichage des posts

--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,10 +1,8 @@
-<div>
-    <div class="relative w-full h-64 sm:h-96 lg:w-1/2 lg:h-auto">
-        @foreach ($posts as $post)
+<div class="relative w-full h-64 sm:h-96 lg:w-1/2 lg:h-auto">
+    @foreach ($posts as $post)
+
+    <livewire:post :post="$post" :key="$post->id"/>
+
     
-        <livewire:post :post="$post" :key="$post->id"/>
-    
-    
-        @endforeach
-    </div>
+    @endforeach
 </div>
